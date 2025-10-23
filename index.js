d05b95aa1c8d7b50fa2c8ba5abce35077dc10e15
Tom Fili <tom@cesium.com>
2025-10-22 23:37:13 -0400

    Read base url directly

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 const images = ["images/Fili1.jpeg", "images/Fili2.jpeg"];
 let currentImageIndex = 0;
 
-async function startSlideshow(id, baseUrl) {
-    baseUrl = baseUrl ?? "";
+async function startSlideshow(id) {
+    const base = document.getElementsByTagName("base")[0];
+    const baseUrl = base?.href ?? "";
     const imgElement = document.getElementById(id);
     imgElement.src = images[currentImageIndex];
     document.body.appendChild(imgElement);
