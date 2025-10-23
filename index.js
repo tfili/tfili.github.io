d56ea3b656fe3e74ba79aa4b8449940a573a27e6
Tom Fili <tom@cesium.com>
2025-10-22 21:38:40 -0400

    Initial homepage

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const images = ["images/Fili1.jpeg", "images/Fili2.jpeg"];
+let currentImageIndex = 0;
+
+async function startSlideshow(id) {
+    const imgElement = document.getElementById(id);
+    imgElement.src = images[currentImageIndex];
+    document.body.appendChild(imgElement);
+
+    while (true) {
+        await delay(3000);
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        imgElement.src = images[currentImageIndex];
+    }
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
